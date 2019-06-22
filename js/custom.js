63053eecbe66c823200a63be27dad8d813d2116e
factory011 <13100721033@139.com>
2019-06-22 09:14:38 +0800

    modify

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,27 @@
 /**
+ * back to top
+ */
+var backToTop = function() {
+  const $backToTop = $('#back-to-top');
+
+  $(window)
+    .scroll(function() {
+      if ($(window)
+        .scrollTop() > 100) {
+        $backToTop.fadeIn(1000);
+      } else {
+        $backToTop.fadeOut(1000);
+      }
+    })
+
+  $backToTop.click(function() {
+    $('body,html')
+      .animate({
+        scrollTop: 0
+      });
+  })
+}
+/**
  * Table of Content fix
  */
 function initToc() {
@@ -37,4 +60,8 @@ var toc = function() {
   }
 }
 
-toc();
+/* main */
+$(document).ready(function () {
+  backToTop();
+  toc();
+});
